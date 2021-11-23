6406d02c0f7bbac14a1a0f1a3b5ac002845cb5f3
novodias <nil.diasbessa@gmail.com>
2021-11-23 13:40:05 -0300

    código nao usado

--- JogoTurnBased/Player.cs
+++ JogoTurnBased/Player.cs
@@ -17,35 +17,35 @@ namespace JogoTurnBased
             return name;
         }
 
-        private bool bFindMonster()
-        {
-            int RandomEncounter = RandomNumber.Next(1, 10);
-            if(RandomEncounter >= 1)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
+        //private bool bFindMonster()
+        //{
+        //    int RandomEncounter = RandomNumber.Next(1, 10);
+        //    if(RandomEncounter >= 1)
+        //    {
+        //        return true;
+        //    }
+        //    else
+        //    {
+        //        return false;
+        //    }
+        //}
 
-        public void FindMonsters()
-        {
-            if(bFindMonster() == true)
-            {
-                Console.WriteLine("Você encontrou um monstro!");
-            }
-            else
-            {
-                Console.WriteLine("Você encontrou nada para batalhar, continuar andando?");
-                string Continue = Console.ReadLine();
-                if (Continue == "andar")
-                {
-                    FindMonsters();
-                }
-            }
-        }
+        //public void FindMonsters()
+        //{
+        //    if(bFindMonster() == true)
+        //    {
+        //        Console.WriteLine("Você encontrou um monstro!");
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("Você encontrou nada para batalhar, continuar andando?");
+        //        string Continue = Console.ReadLine();
+        //        if (Continue == "andar")
+        //        {
+        //            FindMonsters();
+        //        }
+        //    }
+        //}
         
     }
 }
